60d0440bdf2d1555c2046be4084058aa651b4beb
JustinWard <gkingof9@gmail.com>
2021-05-27 21:03:37 -0400

    updates

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 # Imports
 import pygame
 import random
-
+import json 
 
 # Window settings
 GRID_SIZE = 64
-WIDTH = 29 * GRID_SIZE
-HEIGHT = 14 * GRID_SIZE
-TITLE = "Game Title"
+WIDTH = 25 * GRID_SIZE
+HEIGHT = 16 * GRID_SIZE
+TITLE = "Zeladon"
 FPS = 60
 
 
@@ -28,6 +28,8 @@ GRAY = (175,175,175)
 Start = 0
 Playing = 1
 Lose = 2
+Level_complete = 3
+win = 4
 # Load fonts
 font_xl = pygame.font.Font('assets/fonts/Dinomouse-Regular.otf', 96)
 font_lg = pygame.font.Font('assets/fonts/Dinomouse-Regular.otf', 64)
@@ -36,25 +38,52 @@ font_sm = pygame.font.Font('assets/fonts/Dinomouse-Regular.otf', 24)
 font_xs = pygame.font.Font(None, 14)
 
 # Load images
-hero_img = pygame.image.load('assets/images/characters/player_idle.png').convert_alpha()
+hero_imgs_rt = [pygame.image.load('assets/Items/Players/Variable sizes/Blue/alienBlue_walk1.png').convert_alpha(),
+                pygame.image.load('assets/Items/Players/Variable sizes/Blue/alienBlue_walk2.png').convert_alpha()]
+
+hero_jump_rt = [pygame.image.load('assets/Items/Players/Variable sizes/Blue/alienBlue_jump.png').convert_alpha()]
+
+hero_climb_rt = [pygame.image.load('assets/Items/Players/Variable sizes/Blue/alienBlue_climb1.png').convert_alpha(),
+                 pygame.image.load('assets/Items/Players/Variable sizes/Blue/alienBlue_climb2.png').convert_alpha()]
+
+
+hero_imgs_lt = [pygame.transform.flip(img, True, False)for img in hero_imgs_rt]
+hero_jump_lt = [pygame.transform.flip(img, True, False)for img in hero_jump_rt]
+hero_climb_lt = [pygame.transform.flip(img, True, False)for img in hero_climb_rt]
+
+Block_img = pygame.image.load('assets/images/tiles/block.png').convert_alpha()
 stoneBlock_img = pygame.image.load('assets/images/tiles/stone.png').convert_alpha()
 grassBlock_img = pygame.image.load('assets/images/tiles/grass_dirt.png').convert_alpha()
-gems_img = pygame.image.load('assets/images/items/gem.png')
-orangeblade_img = pygame.image.load('assets/Items/images/characters/enemy2a.png').convert_alpha()
-barnacle_img = pygame.image.load('assets/Items/Enemies/barnacle.png').convert_alpha()
-mouse_img = pygame.image.load('assets/Items/Enemies/mouse.png').convert_alpha()
-saw_img = pygame.image.load('assets/Items/Enemies/saw.png').convert_alpha()
-greenworm_img = pygame.image.load('assets/Items/Enemies/wormGreen.png').convert_alpha()
-heart_img = pygame.image.load('assets/images/items/heart.png')
+gems_img = pygame.image.load('assets/images/items/gem.png').convert_alpha()
+heart_img = pygame.image.load('assets/images/items/heart.png').convert_alpha()
+chain_img = pygame.image.load('assets/Items/Tiles/signExit.png').convert_alpha()
+
+bee_imgs = [pygame.image.load('assets/Items/Enemies/bee.png').convert_alpha(),
+            pygame.image.load('assets/Items/Enemies/bee_move.png').convert_alpha()]
+
+barnacle_imgs = [pygame.image.load('assets/Items/Enemies/barnacle.png').convert_alpha(),
+                 pygame.image.load('assets/Items/Enemies/barnacle_attack.png').convert_alpha()]
+                 
+mouse_imgs_rt = [pygame.image.load('assets/Items/Enemies/mouse.png').convert_alpha(),
+              pygame.image.load('assets/Items/Enemies/mouse_move.png').convert_alpha()]
+mouse_imgs_lt = [pygame.transform.flip(img, True, False)for img in mouse_imgs_rt ]
+
+saw_imgs = [pygame.image.load('assets/Items/Enemies/saw.png').convert_alpha(),
+            pygame.image.load('assets/Items/Enemies/saw_move.png').convert_alpha()]
+
+greenworm_imgs = [pygame.image.load('assets/Items/Enemies/wormGreen.png').convert_alpha(),
+                  pygame.image.load('assets/Items/Enemies/wormGreen_move.png').convert_alpha()] 
 
 
 
 # Load sounds
 
 
-#Settings
-gravity = 1.0
-terminal_velocity = 24
+#load levels
+
+Levels = ["assets/levels/World-1.json",
+          "assets/levels/World-2 .json",
+          "assets/levels/World-3.json"]
 
 # Game classes
 
@@ -75,11 +104,33 @@ class Entity (pygame.sprite.Sprite):
          self.vy += gravity
          if self.vy > terminal_velocity:
             self.vy = terminal_velocity
+class AminatedEntity(Entity):
+     def __init__(self, x, y, images):
+         super().__init__(x,y, images[0])
+
+         self.images = images
+         self.image_index = 0
+         self.ticks = 0
+         self.animation_speed = 10
+
+     def set_img_list(self):
+         self.images = self.images 
+     def animate(self):
+         self.set_img_list()
+         self.ticks += 1
+
+         if self.ticks % self.animation_speed == 0:
+              self.image_index += 1
+
+              if self.image_index >= len(self.images):
+                  self.image_index = 0
+                          
+              self.image = self.images[self.image_index]            
+         
+class Hero(AminatedEntity):
     
-class Hero(Entity):
-    
-    def __init__(self, x, y, image):
-        super().__init__(x, y, image)
+    def __init__(self, x, y, images):
+        super().__init__(x, y, images)
         
        
 
@@ -90,13 +141,16 @@ class Hero(Entity):
         self.vy = 0
         self.hurt_timer = 0 
 
-        self.hearts = 3
+        self.hearts = 400
         self.gems = 0
         self.score = 0
        
     def move_right(self):
     	self.vx = self.speed
-    	
+    
+    def move_to(self,x, y):
+        self.rect.centerx = x * GRID_SIZE + GRID_SIZE//2
+        self.rect.centery = y * GRID_SIZE + GRID_SIZE//2
     def move_left(self):
     	self.vx = -1 * self.speed
 
@@ -172,12 +226,15 @@ class Hero(Entity):
     def check_world_edges(self):
         if self.rect.left < 0:
             self.rect.left = 0
-        elif self.rect.right > WIDTH:
-            self.rect.right = WIDTH
+        elif self.rect.right > world_width:
+            self.rect.right = world_width
 
 
         
         
+    def reached_goal(self):
+        return pygame.sprite.spritecollideany(self, goal)
+
     
     def update(self):
         self.apply_gravity()
@@ -185,9 +242,20 @@ class Hero(Entity):
         self.check_world_edges()
         self.check_items()
         self.check_enemy()
+        self.animate()
     	
     	 
 class Platform(Entity):
+    
+    def __init__(self, x, y, image):
+        super().__init__(x, y, image)
+        
+class Blocks(Entity):
+    
+    def __init__(self, x, y, image):
+        super().__init__(x, y, image)
+                              
+class Chain(Entity):
     
     def __init__(self, x, y, image):
         super().__init__(x, y, image)
@@ -202,9 +270,9 @@ class Item(Entity):
         character.score +=10
         print(character.score)
 
-class Enemy(Entity):
-    def __init__(self, x, y, image):
-        super().__init__(x, y, image)
+class Enemy(AminatedEntity):
+    def __init__(self, x, y, images):
+        super().__init__(x, y, images)
 
         
         self.vx = -2
@@ -240,8 +308,8 @@ class Enemy(Entity):
         if self.rect.left < 0:
             self.rect.left = 0
             self.reverse()
-        elif self.rect.right > WIDTH:
-            self.rect.right = WIDTH
+        elif self.rect.right > world_width:
+            self.rect.right = world_width
             self.reverse()
     def check_platform_edges(self):
         self.rect.y += 2
@@ -261,34 +329,42 @@ class Enemy(Entity):
 
 class Saw(Enemy):
     
-    def __init__(self, x, y, image):
-        super().__init__(x, y, image)
+    def __init__(self, x, y, images):
+        super().__init__(x, y, images)
 
     def update(self):
         self.move_and_check_blocks()
         self.apply_gravity()
         self.check_world_edges()
-
+        self.check_platform_edges()
+        self.animate()
 
 class Mouse(Enemy):
     
-    def __init__(self, x, y, image):
-        super().__init__(x, y, image)
+    def __init__(self, x, y, images):
+        super().__init__(x, y, images)
+        
+    def set_image_list(self):
+        if self.vx > 0:
+            self.images = Mouse_imgs_rt
 
+        else:
+            self.images = spick_man_imgs_lt
     def update(self):
         self.move_and_check_blocks()
         self.apply_gravity()
         self.check_world_edges()        
         self.check_platform_edges()
-class Worm(Enemy):
+        self.animate()                  
+class Bee(Enemy):
     
-    def __init__(self, x, y, image):
-        super().__init__(x, y, image)
+    def __init__(self, x, y, images):
+        super().__init__(x, y, images)
 
     def update(self):
         self.move_and_check_blocks()
         self.check_world_edges()        
-        
+        self.animate()
 
 
 # Helper functoins
@@ -308,6 +384,23 @@ def show_lose_screen():
         rect.midbottom = WIDTH // 2, HEIGHT // 2
         screen.blit(text, rect)
         
+        text = font_sm.render("Press 'r' to play again", True, WHITE)
+        rect = text.get_rect()
+        rect.midtop = WIDTH // 2, HEIGHT // 2
+        screen.blit(text, rect)
+def show_win_screen():
+        text = font_xl.render('Level complete!', True, WHITE)
+        rect = text.get_rect()
+        rect.midbottom = WIDTH // 2, HEIGHT // 2
+        screen.blit(text, rect)
+
+       
+def show_wins_screen():
+        text = font_xl.render('You winnnnnnn!', True, WHITE)
+        rect = text.get_rect()
+        rect.midbottom = WIDTH // 2, HEIGHT // 2
+        screen.blit(text, rect)
+
         text = font_sm.render("Press 'r' to play again", True, WHITE)
         rect = text.get_rect()
         rect.midtop = WIDTH // 2, HEIGHT // 2
@@ -351,76 +444,85 @@ def draw_grid(offset_x=0, offset_y=0):
 
    
 # Game loop
+def start_game():
+    global hero, stage, current_level
+    
+    hero = Hero(0, 0, hero_imgs_rt)
+    stage = Start
+    current_level = 2
 def setup():
-    global player, emenys, items, blocks, stage, hero
+    global player, emenys, items, blocks, goal, gravity, terminal_velocity, world_width,  world_height, all_sprites
     
     blocks = pygame.sprite.Group()
-    items = pygame.sprite.Group()
+    items  = pygame.sprite.Group()
     emenys = pygame.sprite.Group()
-    player = pygame.sprite.GroupSingle()
+    player = pygame.sprite.Group()
+    goal   = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
 
-    item_locs = [[11,7], [16,0], [8,3]]
-    block_locs =  [[0,13], [1,13], [2,13], [3,13], [4,13], [5,13],
-                   [6,13], [7,13], [8,13], [9,13], [10,13], [11,13],
-                   [12,13], [13,13], [14,13], [15,13], [16,13], [17,13],
-                   [18,13], [19,13], [20,13], [21,13], [22,13], [23,13],
-                   [24,13], [25,13], [26,13], [27,13], [28,13]]
+    with open(Levels[current_level]) as f:
+        data = json.load(f)
+  
+    goal.add(Chain(data["chain_locs"][0], data["chain_locs"][1], chain_img))
 
-    platform_locs = [[11,6], [12,6], [13,6], [4,10], [5,10], 
-                     [8, 4], [7,4], [9,4], [8,7], [6,10],
-                     [7,10], [11,1], [16,1], [16,11], [17,11],
-                     [18,11], [20,8], [22,5], [20,3], [19,8]]
-    saw_locs = [(11,7)]
-    mouse_locs = [(7,2)]
-    greenworm_locs = [(8,0)]
-
-
-                     
-    for loc in platform_locs:
+    
+             
+    for loc in data ["platform_locs"]:
         x = loc[0]
         y = loc[1]
         b = Platform(x,y,stoneBlock_img)
         blocks.add(b)               
-    for loc in block_locs:
+    for loc in data ["block_locs"]:
         x = loc[0]
         y = loc[1]
         b = Platform(x,y,grassBlock_img)
         blocks.add(b)
-    for loc in item_locs:
+    for loc in data ["item_locs"]:
         x = loc[0]
         y = loc[1]
         b = Item(x,y,gems_img)
         items.add(b)
 
-    for loc in saw_locs:
+    for loc in data ["saw_locs"]:
         x = loc[0]
         y = loc[1]
-        e = Saw(x,y,saw_img)
+        e = Saw(x,y,saw_imgs)
         emenys.add(e)
-    for loc in mouse_locs:
+    for loc in data ["mouse_locs"]:
         x = loc[0]
         y = loc[1]
-        e = Mouse(x,y,mouse_img)
+        e = Mouse(x,y,mouse_imgs_lt)
         emenys.add(e)
 
-    for loc in greenworm_locs:
+    for loc in data ["bee_locs"]:
         x = loc[0]
         y = loc[1]
-        e = Worm(x,y,greenworm_img)
+        e = Bee(x,y,bee_imgs)
         emenys.add(e)
 
     
-    start_x = 0
-    start_y = 0
-
-    hero = Hero(start_x, start_y, hero_img)
-    player.add(hero)
+    for loc in data ["grayBlocks_locs"]:
+        x = loc[0]
+        y = loc[1]
+        b = Blocks(x,y,Block_img)
+        blocks.add(b)
         
+    world_width = data['width'] * GRID_SIZE
+    world_height = data['height'] * GRID_SIZE
+    print(world_width)
+    hero.move_to(data ["start"][0],data ["start"][1])
+    player.add(hero)
 
-    stage = Start
+    gravity = data["gravity"]
+    terminal_velocity = data["terminal_velocity"]
+
+
+    all_sprites.add(player,emenys, items, blocks, goal)
+
 # Input handling
 grid_on = False
 running = True
+start_game()
 setup()
 
 while running:
@@ -435,8 +537,9 @@ while running:
             if stage == Playing:
                   if event.key == pygame.K_SPACE:
                         hero.jump()
-            if stage == Lose:
-                if event.key == pygame.k_r:
+            if stage == Lose or stage == win:
+                if event.key == pygame.K_r:
+                        start_game()
                         setup()
                 
             if event.key == pygame.K_g:
@@ -456,28 +559,57 @@ while running:
     
     # Game logic
     if stage == Playing:
-     player.update()
-     emenys.update()
-    if hero.hearts == 0:
+         player.update()
+         emenys.update()
+         
+         if hero.hearts == 0:
             stage = Lose 
+         elif hero.reached_goal():
+            stage = Level_complete
+            countdown = 2 *FPS
+    elif stage == Level_complete:
+        countdown -= 1
+        if countdown <=0:
+            current_level += 1
+            if current_level <len(Levels):
+                setup()
+                stage = Playing
+            else:
+                stage = win 
+    if hero.rect.centerx < WIDTH // 2:
+        offset_x = 0
+    elif hero.rect.centerx > world_width - WIDTH //2:
+        offset_x = world_width - WIDTH
+    else:
+        offset_x = hero.rect.centerx - WIDTH // 2
+
         
+    if hero.rect.centery < HEIGHT :
+        offset_y =0
+   
+    else:
+        offset_y = hero.rect.centery - HEIGHT //2
     # Drawing code
     screen.fill(SKY_BLUE)
-    player.draw(screen)
-    blocks.draw(screen)
-    emenys.draw(screen)
-    items.draw(screen)
+    
+
+    for sprite in all_sprites:
+        screen.blit(sprite.image, [sprite.rect.x - offset_x, sprite.rect.y-offset_y])
+        
+    
+
     show_hud()
     if grid_on:
-         draw_grid(offset_x=0)
-       
-
+         draw_grid(offset_x)
     if stage == Start:
         show_start_screen()
 
     elif stage == Lose:
         show_lose_screen()
-        
+    elif stage == Level_complete:
+        show_win_screen()
+    elif stage == win:
+        show_wins_screen()
     # Update screen
     pygame.display.update()
 
